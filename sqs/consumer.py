3ca2aa556c4614b11756ab2a369bdd2d0cef931f
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-04-16 08:38:02 +0200

    feat: add ApproximateReceiveCount

--- sqs/consumer.py
+++ sqs/consumer.py
@@ -19,6 +19,12 @@ def sqs_receive_and_delete_message(sqs_url):
         receipt_handle = sqs_message["ReceiptHandle"]
         payload = json.loads(sqs_message["Body"])
         sent_timestamp = sqs_message["Attributes"]["SentTimestamp"]
+        receive_count = sqs_message["Attributes"]["ApproximateReceiveCount"]
+        # Process SQS message before deleting it from the queue
+        # On SQS message processing failure
+        #     if receive_count > SQS_MAX_MESSAGE_RETRIES:
+        #         sqs.delete_message(QueueUrl=SQS_URL, ReceiptHandle=receipt_handle)
+        # On SQS message processing success
         sqs.delete_message(QueueUrl=SQS_URL, ReceiptHandle=receipt_handle)
         message = {"payload": payload, "meta": {"message_id": message_id, "sent_timestamp": sent_timestamp}}
         return message
