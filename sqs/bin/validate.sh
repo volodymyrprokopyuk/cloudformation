71035e3187d518f87c3d38cbeb5f36c7970dac03
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-04-18 22:48:44 +0200

    feat(sqs): add test_producer.py

--- sqs/bin/validate.sh
+++ sqs/bin/validate.sh
@@ -3,9 +3,14 @@
 set -eux
 
 SOURCE=sqs/*.py
+TEST_SOURCE=sqs/test/*.py
 
 aws cloudformation validate-template --template-body file://$(pwd)/sqs.yaml
+
 flake8 $SOURCE
 bandit $SOURCE
 radon cc $SOURCE
-pylint --max-line-length=120 $SOURCE
+pylint --max-line-length=120 $SOURCE || true
+
+flake8 $TEST_SOURCE
+pylint --max-line-length=120 $TEST_SOURCE || true
--- sqs/sqs/test/test_producer.py
+++ sqs/sqs/test/test_producer.py
@@ -1,5 +1,20 @@
-import sqs.producer
+import json
+from unittest.mock import MagicMock, patch
+from pytest import fixture
+from sqs.producer import sqs_send_message
 
 
-def test_ok():
-    pass
+@fixture
+def sqs_message():
+    return {"name": "Vlad"}
+
+
+@patch("boto3.client")
+def test_sqs_send_message_success(mocked_boto3_client, sqs_message):
+    mocked_sqs_client = MagicMock()
+    mocked_sqs_client.send_message.return_value = {"MessageId": 1}
+    mocked_boto3_client.return_value = mocked_sqs_client
+    message_id = sqs_send_message("SQS_URL", sqs_message)
+    mocked_boto3_client.assert_called_once_with("sqs")
+    mocked_sqs_client.send_message.assert_called_once_with(QueueUrl="SQS_URL", MessageBody=json.dumps(sqs_message))
+    assert message_id == 1
