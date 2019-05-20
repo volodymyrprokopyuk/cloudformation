53fc579f170a76e2c603a84664091ff3359e5998
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-05-20 13:40:30 +0200

    fix(cloudwatch): fix config

--- cloudwatch/ec2-user-data.sh
+++ cloudwatch/ec2-user-data.sh
@@ -5,13 +5,14 @@ set -eu
 CLOUDWATCH_AGENT_URL=https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 CLOUDWATCH_AGENT_PACKAGE=${CLOUDWATCH_AGENT_URL##*/}
 CLOUDWATCH_AGENT_PATH=/opt/aws/amazon-cloudwatch-agent
-CLOUDWATCH_AGENT_CONFIG_PATH=$CLOUDWATCH_AGENT_PATH/etc/amazon-cloudwatch-agent.json
-CLOUDWATCH_AGENT_CONFIG_FILE=${CLOUDWATCH_AGENT_CONFIG_PATH##*/}
+CLOUDWATCH_AGENT_CONFIG=$CLOUDWATCH_AGENT_PATH/etc/amazon-cloudwatch-agent.json
+CLOUDWATCH_AGENT_CONFIG_PATH=${CLOUDWATCH_AGENT_CONFIG%/*}
+CLOUDWATCH_AGENT_CONFIG_FILE=${CLOUDWATCH_AGENT_CONFIG##*/}
 
 APP_REPO=https://github.com/volodymyrprokopyuk/cloudformation.git
 APP_ROOT=/opt
-APP_DIR=$APP_ROOT/cloudformation/cloudwatch
-APP_LOG_PATH=$APP_DIR/cloudwatch-logging.log
+APP_PATH=$APP_ROOT/cloudformation/cloudwatch
+APP_LOG=$APP_PATH/cloudwatch-logging.log
 
 sudo su
 
@@ -28,15 +29,17 @@ mkdir -p $APP_ROOT
 cd $APP_ROOT
 git clone $APP_REPO
 
-cd $APP_DIR
+cd $APP_PATH
 python3 -m venv pyvenv
 source pyvenv/bin/activate
 pip3 install -r requirements.txt
 
 # Configure and start unified CloudWatch agent
-cp $APP_DIR/$CLOUDWATCH_AGENT_CONFIG_FILE $CLOUDWATCH_AGENT_CONFIG_PATH
+cp $APP_PATH/$CLOUDWATCH_AGENT_CONFIG_FILE $CLOUDWATCH_AGENT_CONFIG_PATH
 $CLOUDWATCH_AGENT_PATH/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 \
-    -c file:$CLOUDWATCH_AGENT_CONFIG_PATH -s
+    -c file:$CLOUDWATCH_AGENT_CONFIG -s
+# Check unified CloudWatch agent status
+# $CLOUDWATCH_AGENT_PATH/bin/amazon-cloudwatch-agent-ctl -a status
 
 # Execute CloudWatch logging application
-python3 main.py > $APP_LOG_PATH 2>&1
+python3 main.py > $APP_LOG 2>&1
