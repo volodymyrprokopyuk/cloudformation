882225c6da0a1eadbd0b40cdaeb99b067ed4d7f5
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-05-20 13:09:23 +0200

    fix(cloudwatch): fix ec2-user-data.sh

--- cloudwatch/ec2-user-data.sh
+++ cloudwatch/ec2-user-data.sh
@@ -22,9 +22,6 @@ yum install -y python3 git
 # Install unified CloudWatch agent
 curl -O $CLOUDWATCH_AGENT_URL
 rpm -U ./$CLOUDWATCH_AGENT_PACKAGE
-cp $APP_DIR/$CLOUDWATCH_AGENT_CONFIG_FILE $CLOUDWATCH_AGENT_CONFIG_PATH
-$CLOUDWATCH_AGENT_PATH/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 \
-    -c file:$CLOUDWATCH_AGENT_CONFIG_PATH -s
 
 # Install CloudWatch logging application
 mkdir -p $APP_ROOT
@@ -35,6 +32,11 @@ cd $APP_DIR
 python3 -m venv pyvenv
 source pyvenv/bin/activate
 pip3 install -r requirements.txt
+
+# Configure and start unified CloudWatch agent
+cp $APP_DIR/$CLOUDWATCH_AGENT_CONFIG_FILE $CLOUDWATCH_AGENT_CONFIG_PATH
+$CLOUDWATCH_AGENT_PATH/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 \
+    -c file:$CLOUDWATCH_AGENT_CONFIG_PATH -s
 
 # Execute CloudWatch logging application
 python3 main.py > $APP_LOG_PATH 2>&1
